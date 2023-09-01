82a95f36d58d7dba39de7b27c2f4b04acf897ee3
AlexRubenko <alexrubenko80@gmail.com>
2023-09-01 17:00:01 +0300

    Lesson_23 done. Created index.htm, get_documents.html, view's etc

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django_extensions",
     "phonenumber_field",
+    "crispy_forms",
     "account",
     "document_flow",
 ]
@@ -88,3 +89,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+# settings.py
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- src/document_flow/views.py
+++ src/document_flow/views.py
@@ -1,3 +1,60 @@
+from django.db.models import Q
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from webargs import fields
+from webargs.djangoparser import use_args
+
+from document_flow.models import CADDocument
+
+
+def index(request):
+    return render(request, 'document_flow/index.html')
+
+
+@use_args(
+    {
+        "document_number": fields.Str(
+            required=False,
+        ),
+        "document_name": fields.Str(
+            required=False,
+        ),
+        "search": fields.Str(
+            required=False,
+        ),
+    },
+    location="query"
+)
+def get_documents(request, params):
+    print(params)
+    documents = CADDocument.objects.all()
+
+    search_fields = ["document_number", "document_name"]
+
+    for param_name, param_value in params.items():
+        if param_name == "search":
+            or_filter = Q()
+            for field in search_fields:
+                or_filter |= Q(**{F"{field}__icontains": param_value})
+            documents = documents.filter(or_filter)
+        else:
+            documents = documents.filter(**{param_name: param_value})
+
+    return render(request,
+                  template_name="document_flow/documents_list.html",
+                  context={"documents": documents})
+
+
+@csrf_exempt
+def update_document(request, pk):
+    pass
+
+
+@csrf_exempt
+def delete_document(request, pk):
+    pass
+
+
+def custom_404_view(request, exception):
+    return render(request, '404.html', status=404)
