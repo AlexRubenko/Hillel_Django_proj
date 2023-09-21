9f30e49f62c550e8ba018c96f963dc4f5f0e3090
AlexRubenko <alexrubenko80@gmail.com>
2023-09-21 15:15:14 +0300

    Lesson_26 done. Added dockerfile ...

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
 
-import document_flow
-from document_flow.urls import index
+from document_flow.views import index
 
 handler404 = 'document_flow.views.custom_404_view'
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('document_flow.urls')),
+    path("", index, name='index'),
     path('api-auth/', include('rest_framework.urls')),
     path("api/", include('api.urls')),
     path('document_flow/', include('document_flow.urls'))
